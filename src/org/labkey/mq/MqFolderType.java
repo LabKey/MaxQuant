f6af97467263c391b59badab664111e2bb7de84c
vagisha <vagisha@gmail.com>
2016-05-02 14:18:19 -0700

    files missing from previous commit

---
+++ src/org/labkey/mq/MqFolderType.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2012-2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.mq;
+
+import org.jetbrains.annotations.NotNull;
+import org.labkey.api.data.Container;
+import org.labkey.api.module.Module;
+import org.labkey.api.module.ModuleProperty;
+import org.labkey.api.module.MultiPortalFolderType;
+import org.labkey.api.util.HelpTopic;
+import org.labkey.api.view.ActionURL;
+import org.labkey.api.view.NavTree;
+import org.labkey.api.view.Portal;
+import org.labkey.api.view.ViewContext;
+import org.labkey.mq.view.search.ProteinSearchWebPart;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * User: vsharma
+ * Date: 4/1/12
+ * Time: 10:58 AM
+ */
+public class MqFolderType extends MultiPortalFolderType
+{
+    public static final String NAME = "MaxQuant";
+
+    public MqFolderType(MqModule module)
+    {
+        super(NAME,
+                "Manage results from MaxQuant runs.",
+            Collections.<Portal.WebPart>emptyList(),
+            Arrays.asList(
+                    Portal.getPortalPart(ProteinSearchWebPart.NAME).createWebPart(),
+                    Portal.getPortalPart("Experiment Runs").createWebPart(),
+                    Portal.getPortalPart("Data Pipeline").createWebPart()
+            ),
+            getDefaultModuleSet(module, getModule("mq"), getModule("Pipeline"), getModule("Experiment")),
+            module);
+    }
+
+    @Override
+    public HelpTopic getHelpTopic()
+    {
+        return new HelpTopic("MaxQuant");
+    }
+
+    @Override
+    public String getLabel()
+    {
+        return "MaxQuant";
+    }
+
+     @Override
+    public String getStartPageLabel(ViewContext ctx)
+    {
+        return "MaxQuant Dashboard";
+    }
+}
---
+++ src/org/labkey/mq/parser/Constants.java
@@ -0,0 +1,18 @@
+package org.labkey.mq.parser;
+
+/**
+ * Created by vsharma on 5/2/2016.
+ */
+public class Constants
+{
+    // SILAC file columns
+    private static final String HL = "H/L";
+    private static final String HM = "H/M";
+    private static final String ML = "M/L";
+    static final String[] RatioTypes = new String[] {HL, HM, ML};
+    private static final String H = "H";
+    private static final String M = "M";
+    private static final String L = "L";
+    static final String[] LabelTypes = new String[] {H, M, L};
+    static final String Ratio = "Ratio";
+}
