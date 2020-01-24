b85acf092e95374be3316670bab4c4641100e05e
Adam Rauch <adam@labkey.com>
2020-01-24 14:30:30 -0800

    Remove "obsolete" directories and scripts (#20)

--- resources/schemas/dbscripts/postgresql/obsolete/mq-18.13-18.14.sql
+++
@@ -1,6 +0,0 @@
-
-DROP INDEX mq.IX_TMTChannel_ExperimentGroupId;
-DROP INDEX mq.IX_ProteinGroupTMT_ProteinGroupId;
-DROP INDEX mq.IX_PeptideTMT_PeptideId;
-DROP INDEX mq.IX_EvidenceTMT_EvidenceId;
-DROP INDEX mq.IX_ModifiedPeptideTMT_ModifiedPeptideId;
